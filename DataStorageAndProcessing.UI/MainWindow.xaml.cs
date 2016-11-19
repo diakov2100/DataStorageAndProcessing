00f167b2892cf596ca6d4df05b0dccf485cb0a10
diakov2100 <diakov2100@gmail.com>
2016-11-19 07:25:41 +0300

    7:25_19.11.16

--- DataStorageAndProcessing.UI/MainWindow.xaml.cs
+++ DataStorageAndProcessing.UI/MainWindow.xaml.cs
@@ -30,11 +30,12 @@ namespace DataStorageAndProcessing.UI
         public MainWindow()
         {
             InitializeComponent();
+            selectyearBox.SelectedIndex = 0;
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            using (var contex= new Context())
+            using (var contex = new Context())
             {
 
                 //contex.Institutions.ToList();
@@ -50,7 +51,7 @@ namespace DataStorageAndProcessing.UI
                 Debug.Write(script);
 
                 ////This will run the migration update script and will run Seed() method
-                migrator.Update();               
+                migrator.Update();
 
             }
         }
@@ -60,6 +61,44 @@ namespace DataStorageAndProcessing.UI
             using (var context = new Context())
             {
                 context.Database.Delete();
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+            using (var context = new Context())
+            {
+                int tecyear = 2012 + selectyearBox.SelectedIndex;
+                var seq = from t1 in context.Raitings
+                          where t1.Year == tecyear 
+                          select t1;
+                IEnumerable<Repository.YearRait> sequence = from t1 in seq
+                                                     join t2 in context.InstiutionRaitings on t1.Id equals t2.RaitingID
+                                                     join t3 in context.Institutions on t2.InstitutionID equals t3.Id
+                                                     join t4 in context.Locations on t3.LocationID equals t4.Id
+                                                     select new Repository.YearRait
+                                                     {
+                                                         WordRank = t2.WordRank,
+                                                         Institution = t3.Name,
+                                                         Location = t4.CountryName,
+                                                         NationalRank = t2.NationalRank,
+                                                         QualityofEducation = t2.QualityofEducation,
+                                                         AlumniEmployment = t2.AlumniEmployment,
+                                                         QualityofFaculty = t2.QualityofFaculty,
+                                                         Publications = t2.Publications,
+                                                         Influence = t2.Influence,
+                                                         Citations = t2.Citations,
+                                                         BroadImpact = t2.BroadImpact,
+                                                         Patents = t2.Patents,
+                                                         Score = t2.Score
+                                                     };
+                //dataGrid.ItemsSource = sequence;
+                foreach (Repository.YearRait tec in sequence)
+                {
+                    dataGrid.Items.Add(tec);
+                }
             }
         }
     }
---
+++ DataStorageAndProcessing.UI/Repository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStorageAndProcessing.UI
+{
+    public class Repository
+    {
+        public class YearRait
+        {
+            public int WordRank { get; set; }
+            public string Institution{ get; set; }
+            public string Location { get; set; }
+            public int NationalRank { get; set; }
+            public int QualityofEducation { get; set; }
+            public int AlumniEmployment { get; set; }
+            public int QualityofFaculty { get; set; }
+            public int Publications { get; set; }
+            public int Influence { get; set; }
+            public int Citations { get; set; }
+            public int BroadImpact { get; set; }
+            public int Patents { get; set; }
+            public double Score { get; set; }
+        }
+    }
+}
